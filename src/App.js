dfde9eb6fae1fc3ee44e0493d6bda3182c274c70
jswan <jswannieman@gmail.com>
2023-08-22 12:17:35 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useReducer, useRef } from "react";
+import React, { useState, useReducer, useRef } from "react";
 
 const App = () => {
+  const [inputValues, setInputValues] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
   const inputRef = useRef();
   const [tech, dispatch] = useReducer((state, action) => {
     switch (action.type) {
@@ -23,10 +25,18 @@ const App = () => {
 
   function handleSubmit(e) {
     e.preventDefault();
-    dispatch({
-      type: "add",
-      description: inputRef.current.value,
-    });
+    if (inputValues.includes(inputRef.current.value)) {
+      setErrorMessage(
+        "You have already chosen that technology - Please choose another technology"
+      );
+    } else {
+      setInputValues([...inputValues, inputRef.current.value]);
+      setErrorMessage("");
+      dispatch({
+        type: "add",
+        description: inputRef.current.value,
+      });
+    }
     inputRef.current.value = "";
   }
 
@@ -57,6 +67,7 @@ const App = () => {
           </li>
         ))}
       </ul>
+      {errorMessage.length > 0 && <p>{errorMessage}</p>}
     </div>
   );
 };
